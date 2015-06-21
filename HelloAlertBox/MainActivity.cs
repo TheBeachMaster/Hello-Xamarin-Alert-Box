4dae08d7c2b386de68ff7c26d50d9b5622cc731b
Arthur Kennedy Otieno <otienoken@outlook.com>
2015-06-21 16:31:18 +0300

    These are all the poject files

---
+++ HelloAlertBox/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAlertBox
+{
+    [Activity(Label = "HelloAlertBox", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            //Just Got rid of the default template.Nothing much.
+            //We'll hook our button to do stuff!!!
+
+            var myButton = FindViewById<Button>(Resource.Id.buttonClick);//Solved it. VS is just not quickly catching up
+
+            myButton.Click += delegate{
+                new AlertDialog.Builder(this)
+                .SetPositiveButton("OK",delegate
+                {
+                    //This code will just log to our console
+                    // here's a snippet for console.WritelIne
+                    Console.WriteLine("OK Button Clicked");
+                })
+                .SetNegativeButton("Cancel",delegate{
+                Console.WriteLine("Cancel clicked");
+                })
+                .SetMessage("That was a rough one \nfor Android")
+                .SetTitle("Xamarin and C# rocks")
+                .Show();
+            };
+            }
+
+
+        }
+    }
+
---
+++ HelloAlertBox/obj/Debug/android/src/md531b817cf3858e1268cc0c02e1e8dad25/MainActivity.java
@@ -0,0 +1,46 @@
+package md531b817cf3858e1268cc0c02e1e8dad25;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("HelloAlertBox.MainActivity, HelloAlertBox, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("HelloAlertBox.MainActivity, HelloAlertBox, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
